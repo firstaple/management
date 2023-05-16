93559fb8f0d292c9691d6b5c1dac33694d5e7860
firstaple <firstaple@gmail.com>
2023-05-16 14:22:36 +0900

    Hiding and Showing Components

--- src/projects/ProjectList.tsx
+++ src/projects/ProjectList.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { Project } from "./Project";
 import ProjectCard from "./ProjectCard";
 import ProjectForm from "./ProjectForm";
@@ -7,15 +8,20 @@ interface ProjectListProps {
 }
 
 const ProjectList = ({ projects }: ProjectListProps) => {
+  const [projectBeingEdited, setProjectBeingEdited] = useState({});
+
   const handleEdit = (project: Project) => {
-    console.log(project);
+    setProjectBeingEdited(project);
   };
   return (
     <div className="row">
       {projects.map((project) => (
         <div key={project.id} className="cols-sm">
-          <ProjectCard project={project} onEdit={handleEdit} />
-          <ProjectForm />
+          {project === projectBeingEdited ? (
+            <ProjectForm />
+          ) : (
+            <ProjectCard project={project} onEdit={handleEdit} />
+          )}
         </div>
       ))}
     </div>
