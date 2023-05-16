91d94eb609b18b37e4cf75eb85557615e38c80c6
firstaple <firstaple@gmail.com>
2023-05-16 14:42:53 +0900

    More Component Communication

--- src/projects/ProjectForm.tsx
+++ src/projects/ProjectForm.tsx
@@ -1,4 +1,8 @@
-const ProjectForm = () => {
+interface ProjectFormProps {
+  onCancel: () => void;
+}
+
+const ProjectForm = ({ onCancel }: ProjectFormProps) => {
   return (
     <form className="input-group vertical">
       <label htmlFor="name">Project Name</label>
@@ -12,7 +16,7 @@ const ProjectForm = () => {
       <div className="input-group">
         <button className="primary bordered medium">Save</button>
         <span />
-        <button type="button" className="bordered medium">
+        <button type="button" className="bordered medium" onClick={onCancel}>
           cancel
         </button>
       </div>
--- src/projects/ProjectList.tsx
+++ src/projects/ProjectList.tsx
@@ -13,12 +13,16 @@ const ProjectList = ({ projects }: ProjectListProps) => {
   const handleEdit = (project: Project) => {
     setProjectBeingEdited(project);
   };
+
+  const cancelEditing = () => {
+    setProjectBeingEdited({});
+  };
   return (
     <div className="row">
       {projects.map((project) => (
         <div key={project.id} className="cols-sm">
           {project === projectBeingEdited ? (
-            <ProjectForm />
+            <ProjectForm onCancel={cancelEditing} />
           ) : (
             <ProjectCard project={project} onEdit={handleEdit} />
           )}
