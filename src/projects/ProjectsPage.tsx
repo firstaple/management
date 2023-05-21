be9cc7e1d58b4e927e3aa1934e7fa2cd3e358686
firstaple <firstaple@gmail.com>
2023-05-21 17:46:45 +0900

    HTTP GET

--- src/projects/ProjectsPage.tsx
+++ src/projects/ProjectsPage.tsx
@@ -1,10 +1,40 @@
-import { useState } from "react";
-import { MOCK_PROJECTS } from "./MockProjects";
+import { useEffect, useState } from "react";
 import { Project } from "./Project";
 import ProjectList from "./ProjectList";
+import { projectAPI } from "./projectAPI";
 
 const ProjectsPage = () => {
-  const [projects, setProjects] = useState<Project[]>(MOCK_PROJECTS);
+  const [projects, setProjects] = useState<Project[]>([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState<string | undefined>(undefined);
+  const [currentPage, setCurrentPage] = useState(1);
+
+  const handleMoreClick = () => {
+    setCurrentPage((currentPage) => currentPage + 1);
+  };
+
+  useEffect(() => {
+    const loadProjects = async () => {
+      setLoading(true);
+      try {
+        // const data = await projectAPI.get(1);
+        const data = await projectAPI.get(currentPage);
+        if (currentPage === 1) {
+          setProjects(data);
+        } else {
+          setProjects((projects) => [...projects, ...data]);
+        }
+        // setError("");
+      } catch (e) {
+        if (e instanceof Error) {
+          setError(e.message);
+        }
+      } finally {
+        setLoading(false);
+      }
+    };
+    loadProjects();
+  }, [currentPage]);
   const saveProject = (project: Project) => {
     console.log("Saving project: ", project);
     let updatedProjects = projects.map((p: Project) => {
@@ -15,7 +45,37 @@ const ProjectsPage = () => {
   return (
     <>
       <h1>Projects</h1>
+      {error && (
+        <div className="row">
+          <div className="card large error">
+            <section>
+              <p>
+                <span className="icon-alert inverse"></span>
+                {error}
+              </p>
+            </section>
+          </div>
+        </div>
+      )}
       <ProjectList onSave={saveProject} projects={projects} />
+
+      {!loading && !error && (
+        <div className="row">
+          <div className="col-sm-12">
+            <div className="button-group fluid">
+              <button className="button default" onClick={handleMoreClick}>
+                More...
+              </button>
+            </div>
+          </div>
+        </div>
+      )}
+      {loading && (
+        <div className="center-page">
+          <span className="spinner primary"></span>
+          <p>Loading...</p>
+        </div>
+      )}
     </>
   );
 };
