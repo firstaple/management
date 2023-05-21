5c3687ac04a709a5676e8d88571c6802bc551762
firstaple <firstaple@gmail.com>
2023-05-22 02:04:20 +0900

    HTTP PUT

--- src/projects/ProjectsPage.tsx
+++ src/projects/ProjectsPage.tsx
@@ -36,11 +36,19 @@ const ProjectsPage = () => {
     loadProjects();
   }, [currentPage]);
   const saveProject = (project: Project) => {
-    console.log("Saving project: ", project);
-    let updatedProjects = projects.map((p: Project) => {
-      return p.id === project.id ? project : p;
-    });
-    setProjects(updatedProjects);
+    projectAPI
+      .put(project)
+      .then((updatedProject) => {
+        let updatedProjects = projects.map((p: Project) => {
+          return p.id === project.id ? new Project(updatedProject) : p;
+        });
+        setProjects(updatedProjects);
+      })
+      .catch((e) => {
+        if (e instanceof Error) {
+          setError(e.message);
+        }
+      });
   };
   return (
     <>
